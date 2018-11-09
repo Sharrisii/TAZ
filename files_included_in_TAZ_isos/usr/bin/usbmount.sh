9f16b586c3ae3585904f3448c99a42ccf2e0755c
S. Lockwood-Childs <sjl@vctlabs.com>
2018-11-09 01:00:58 -0800

    update USB stick automount feature
    create fstab entries for usb sticks that include 'users' option
    so that non-root user has permissions to unmount usb stick
    ('users' option doesn't work when passed directly on mount commandline)
    
    run 'local' service after mounting usb, which has usbmount.sh script
    
    fix some syntax errors and take out long sleeps in usbmount.sh script

--- files_included_in_TAZ_isos/usr/bin/usbmount.sh
+++ files_included_in_TAZ_isos/usr/bin/usbmount.sh
@@ -4,5 +4,10 @@ DEVPATH=$DEVNAME
 DEVNAME=$(basename $DEVNAME)
 MOUNTPOINT=/mnt/$DEVNAME
 
-mkdir -p $MOUNTPOINT
-mount -o sync,nodev,nosuid $DEVPATH $MOUNTPOINT
+if grep -q $MOUNTPOINT /etc/fstab ; then
+    # make use of fstab entry, which has options to allow user to unmount
+    mkdir -p $MOUNTPOINT
+    mount $MOUNTPOINT
+
+    /etc/init.d/local restart
+fi
