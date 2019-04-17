4748eee1876d9e2ce5ef990bf114ca4508fc94e8
Default LiveCD User <taz@livecd.gentoo>
2019-04-17 16:51:11 +0000

    update

--- files_included_in_TAZ_isos/home/taz/.gimp-2.8/scripts/scan-ocr.scm
+++ files_included_in_TAZ_isos/home/taz/.gimp-2.8/scripts/scan-ocr.scm
@@ -1,12 +1,12 @@
-(define (tesseract filename)
+(define (tesseract filename scan_ocr_low_treshold_value scan_ocr_high_treshold_value)
   (let*
     (
       (image (car (gimp-file-load RUN-NONINTERACTIVE filename filename)))
       (drawable (car (gimp-image-get-active-layer image)))
     )
     (gimp-image-convert-grayscale image)
-    (gimp-threshold image LOW HIGH)
-    (gimp-file-save RUN-NONINTERACTIVE image drawable filename filename)
+    (gimp-threshold image scan_ocr_low_treshold_value scan_ocr_high_treshold_value)
+    (gimp-file-save RUN-NONINTERACTIVE image drawable filename tesseract-ocr-output.txt)
     (gimp-image-delete image)
   )
 )
